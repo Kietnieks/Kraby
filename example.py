937cc773ffe21a9e84e2ec7f8c857cd656452273
Carl Farrington <carl@eastshore.com>
2020-09-01 22:07:23 -0700

    Factor key handling out of sprite

--- example.py
+++ example.py
@@ -8,30 +8,52 @@ import pygame
 pygame.init()
 root = sys.path[0]
 
+class KeyCombo:
+    event_types = (pygame.KEYDOWN,pygame.KEYUP)
+    def __init__(self,outputs):
+        self.my_keys = set()
+        self.outputs = []
+        for row in outputs:
+            s = set(row[1:])
+            self.outputs.append((row[0],s))
+            self.my_keys |= s
+        self.active_keys = set()
+    def handle_event(self,event):
+        if event.type not in self.event_types:
+            return
+        if event.key not in self.my_keys:
+            return
+        if event.type == pygame.KEYDOWN:
+            self.active_keys.add(event.key)
+        else:
+            self.active_keys.remove(event.key)
+    def get_value(self):
+        for val,s in self.outputs:
+            if self.active_keys == s:
+                return val
+        return self.outputs[-1][0]
+
 class Sprite:
     def __init__(self,image_file,scale=None):
         self.image = pygame.image.load(image_file)
         if scale:
             self.image = pygame.transform.rotozoom(self.image,0,scale)
         self.rect = self.image.get_rect()
-        self.active_keys = set()
+        self.event_listeners = dict(
+                hmove = KeyCombo([
+                        (-1,pygame.K_a),
+                        (1,pygame.K_d),
+                        (0,),
+                        ])
+                )
     def draw(self,screen):
         speed = 3
-        for key,motion in (
-                (pygame.K_a,[-speed,0]),
-                (pygame.K_d,[speed,0]),
-                ):
-            if self.active_keys == set([key]):
-                self.rect = self.rect.move(motion)
+        hdir = self.event_listeners['hmove'].get_value()
+        self.rect = self.rect.move([speed*hdir,0])
         screen.blit(self.image, self.rect)
     def handle_event(self,event):
-        valid_keys = set([pygame.K_a,pygame.K_d])
-        if event.type == pygame.KEYDOWN:
-            if event.key in valid_keys:
-                self.active_keys.add(event.key)
-        if event.type == pygame.KEYUP:
-            if event.key in valid_keys:
-                self.active_keys.remove(event.key)
+        for el in self.event_listeners.values():
+            el.handle_event(event)
 
 
 size = width, height = 640, 480
