965d613a2db9222d290b4c8fa0f7f38295696e0f
Carl Farrington <carl@eastshore.com>
2020-09-02 10:45:21 -0700

    Add vertical motion
    - Switch to a new key-tracking class that supports multiple independent keys
      and long-press detection
    - separate update logic from drawing logic
    - make speed independent of frame rate
    - pull event handling out of Sprite class
    - remove some dead code

--- example.py
+++ example.py
@@ -3,9 +3,6 @@
 # XXX introduce coustum sprites/backgrounds
 # XXX
 # XXX Internal needs:
-# XXX - update rate control:
-# XXX   - on fast systems, limit the maximum update tick rate
-# XXX   - on slow systems, scale up the move size per tick
 # XXX - background
 # XXX   - when drawing, a horizontal scroll position selects what portion of
 # XXX     the background is blitted onto the screen, and also offsets the
@@ -24,30 +21,23 @@ import pygame
 pygame.init()
 root = sys.path[0]
 
-class KeyCombo:
+class KeyState:
+    '''Track the current state of all keys.
+
+    self.active_keys has an entry for each key currently depressed.
+    The entry holds the key-down time, so long presses can be
+    handled specially.
+    '''
     event_types = (pygame.KEYDOWN,pygame.KEYUP)
-    def __init__(self,outputs):
-        self.my_keys = set()
-        self.outputs = []
-        for row in outputs:
-            s = set(row[1:])
-            self.outputs.append((row[0],s))
-            self.my_keys |= s
-        self.active_keys = set()
-    def handle_event(self,event):
+    def __init__(self):
+        self.active_keys = dict()
+    def handle_event(self,event,now):
         if event.type not in self.event_types:
             return
-        if event.key not in self.my_keys:
-            return
         if event.type == pygame.KEYDOWN:
-            self.active_keys.add(event.key)
+            self.active_keys[event.key] = now
         else:
-            self.active_keys.remove(event.key)
-    def get_value(self):
-        for val,s in self.outputs:
-            if self.active_keys == s:
-                return val
-        return self.outputs[-1][0]
+            del(self.active_keys[event.key])
 
 class Sprite:
     def __init__(self,image_file,scale=None):
@@ -55,21 +45,41 @@ class Sprite:
         if scale:
             self.image = pygame.transform.rotozoom(self.image,0,scale)
         self.rect = self.image.get_rect()
-        self.event_listeners = dict(
-                hmove = KeyCombo([
-                        (-1,pygame.K_a),
-                        (1,pygame.K_d),
-                        (0,),
-                        ])
-                )
+        hpix_per_sec = 200
+        self.hmove = {
+                pygame.K_a:-hpix_per_sec,
+                pygame.K_d:hpix_per_sec,
+                }
     def draw(self,screen):
-        speed = 3
-        hdir = self.event_listeners['hmove'].get_value()
-        self.rect = self.rect.move([speed*hdir,0])
         screen.blit(self.image, self.rect)
-    def handle_event(self,event):
-        for el in self.event_listeners.values():
-            el.handle_event(event)
+    def update(self,now,ms_elapsed,key_state):
+        def scale_speed(speed):
+            '''Return the number of pixels to move in this tick.
+
+            Input is the tick length in ms and the desired speed
+            in pixels per second.
+            '''
+            return speed * ms_elapsed / 1000
+        bounding_box = pygame.Rect(0,0,640,480) #XXX from background eventually
+        long_press_time = 500
+        # vertical motion
+        v = key_state.active_keys.get(pygame.K_w)
+        if v is None:
+            vspeed = 600 # normal fall (gravity)
+        elif now - v > long_press_time:
+            vspeed = 50 # slow fall
+        else:
+            vspeed = -200 # jump
+        new_rect = self.rect.move([0,scale_speed(vspeed)])
+        self.rect = new_rect.clamp(bounding_box)
+        # horizontal motion
+        d = self.hmove
+        l = set([d[x] for x in key_state.active_keys if x in d])
+        if len(l) == 1:
+            hspeed = next(iter(l))
+            hpix = hspeed * ms_elapsed / 1000
+            new_rect = self.rect.move([hpix,0])
+            self.rect = new_rect.clamp(bounding_box)
 
 
 size = width, height = 640, 480
@@ -77,23 +87,23 @@ black = 0, 0, 0
 grey = 50, 50, 50
 screen = pygame.display.set_mode(size)
 
-#player = Sprite("intro_ball.gif")
 player = Sprite(os.path.join(root,"blue goblin.png"),scale=0.25)
+key_state = KeyState()
+event_handlers=[
+        key_state,
+        ]
 
 clock = pygame.time.Clock()
 while 1:
+    ms_elapsed = clock.tick(20)
+    now = pygame.time.get_ticks()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        player.handle_event(event)
+        for eh in event_handlers:
+            eh.handle_event(event,now)
 
-    if False:
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
-
-    ms_elapsed = clock.tick(60)
+    player.update(now,ms_elapsed,key_state)
     screen.fill(grey)
     player.draw(screen)
     pygame.display.flip()
